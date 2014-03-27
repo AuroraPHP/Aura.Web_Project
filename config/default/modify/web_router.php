91657d2eecd7d9d757c6ff705045ee981279d6b4
David Stanley <davidstanley01@gmail.com>
2014-03-27 16:36:43 -0700

    Initial

--- config/default/modify/web_router.php
+++ config/default/modify/web_router.php
@@ -1,23 +1,20 @@
 <?php
 /**
- * 
+ *
  * This file is part of Aura for PHP.
- * 
+ *
  * @package Aura.Web_Project
- * 
+ *
  * @license http://opensource.org/licenses/bsd-license.php BSD
- * 
+ *
  * @var Aura\Di\Container $di The DI container.
- * 
+ *
  */
 
-// get the router service
-$router = $di->get('web_router');
-
 // example route for 'hello world' using request and response services
-$request  = $di->get('web_request');
-$response = $di->get('web_response');
-$router->add('hello', '/')
+$request  = Request::get();
+$response = Response::get();
+Router::add('hello', '/')
     ->addValues(array(
         'controller' => function () use ($request, $response) {
             $response->content->set('Hello World!');
--- web/index.php
+++ web/index.php
@@ -1,17 +1,29 @@
 <?php
 /**
- * 
+ *
  * This file is part of Aura for PHP.
- * 
+ *
  * @package Aura.Web_Project
- * 
+ *
  * @license http://opensource.org/licenses/bsd-license.php BSD
- * 
+ *
  */
+use AuraFacades\Facade;
 
 // invoke the project kernel script
 require dirname(__DIR__) . '/vendor/aura/project-kernel/scripts/kernel.php';
 
 // create and invoke a web kernel
 $web_kernel = $di->newInstance('Aura\Web_Kernel\WebKernel');
+
+// Initialize the facade locator service registry thingy
+Facade::setFacadeApplication($web_kernel);
+
+// Add aliases here if you want to override the default Aura aliases
+// $aliases = array(
+//     '' => '',
+// );
+
+Facade::registerAliases();
+
 $web_kernel();
